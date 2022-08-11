71e106b3426dcb14b82fd0331db06dacff31ce50
lhj502819 <lhj502819@163.com>
2022-08-11 17:16:28 +0800

    调整单元测试类

--- small-spring-step04/README.md
+++ small-spring-step04/README.md
@@ -1,4 +1,13 @@
 # 增加功能
 - 增加在实例化后进行属性填充
 # 缺点
-- 缺少对实例字段的填充，不只是基本数据类型，还包括没有实例化的对象属性，都需要在Bean创建的时候进行填充
+- 使用时都需要手动去硬编码注册Bean，繁琐，实用性较低
+- ```
+  //3.UserService设置属性[uId，userDao]
+  PropertyValues propertyValues = new PropertyValues();
+  propertyValues.addPropertyValue(new PropertyValue("uId" , "1002"));
+  propertyValues.addPropertyValue(new PropertyValue("userDao" , new BeanReference("userDao")));
+
+  //4.UserService注入Bean
+  BeanDefinition beanDefinition = new BeanDefinition(UserService.class, propertyValues);
+  beanFactory.registerBeanDefinition("userService" , beanDefinition);```
--- small-spring-step05/src/test/java/cn/onenine/springframework/ApiTest.java
+++ small-spring-step05/src/test/java/cn/onenine/springframework/ApiTest.java
@@ -1,5 +1,6 @@
 package cn.onenine.springframework;
 
+import cn.hutool.core.io.IoUtil;
 import cn.onenine.springframework.bean.UserService;
 import cn.onenine.springframework.beans.factory.BeanFactory;
 import cn.onenine.springframework.beans.factory.PropertyValue;
@@ -7,9 +8,14 @@ import cn.onenine.springframework.beans.factory.PropertyValues;
 import cn.onenine.springframework.beans.factory.config.BeanDefinition;
 import cn.onenine.springframework.beans.factory.config.BeanReference;
 import cn.onenine.springframework.beans.factory.support.DefaultListableBeanFactory;
+import cn.onenine.springframework.beans.factory.support.XmlBeanDefinitionReader;
+import cn.onenine.springframework.core.io.DefaultResourceLoader;
+import cn.onenine.springframework.core.io.Resource;
 import cn.onenine.springframework.dao.UserDao;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.lang.reflect.Constructor;
 
 /**
@@ -19,27 +25,59 @@ import java.lang.reflect.Constructor;
  */
 public class ApiTest {
 
-    @Test
-    public void test_BeanFactory() {
+    private DefaultResourceLoader resourceLoader;
 
+    @Before
+    public void init() {
+        resourceLoader = new DefaultResourceLoader();
+    }
+
+    @Test
+    public void test_classpath() throws Exception {
+        Resource resource = resourceLoader.getResource("classpath:important.properties");
+
+        InputStream inputStream = resource.getInputStream();
+
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.printf(content);
+    }
+
+    @Test
+    public void test_file() throws Exception {
+        Resource resource = resourceLoader.getResource("src/test/resources/important.properties");
+        InputStream in = resource.getInputStream();
+        String content = IoUtil.readUtf8(in);
+        System.out.printf(content);
+    }
+
+
+    /**
+     * TODO 待测试为何拉取到流是整个html元素
+     *
+     * @throws Exception
+     */
+    @Test
+    public void test_url() throws Exception {
+        Resource resource = resourceLoader.getResource("https://github.com/fuzhengwei/small-spring/blob/main/important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_xml() throws Exception {
         //1.初始化BeanFactory
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 
-        //2.UserDao注册
-        beanFactory.registerBeanDefinition("userDao" , new BeanDefinition(UserDao.class));
+        //2.读取配置文件&注册Bean
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions("classpath:spring.xml");
 
-        //3.UserService设置属性[uId，userDao]
-        PropertyValues propertyValues = new PropertyValues();
-        propertyValues.addPropertyValue(new PropertyValue("uId" , "1002"));
-        propertyValues.addPropertyValue(new PropertyValue("userDao" , new BeanReference("userDao")));
-
-        //4.UserService注入Bean
-        BeanDefinition beanDefinition = new BeanDefinition(UserService.class, propertyValues);
-        beanFactory.registerBeanDefinition("userService" , beanDefinition);
-
-        //5.获取UserService Bean
-        UserService userService = (UserService) beanFactory.getBean("userService");
+        //3.获取Bean对象调用方法
+        UserService userService = beanFactory.getBean("userService", UserService.class);
         userService.queryUserInfo();
+
+
     }
 
 
